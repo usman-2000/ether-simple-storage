579038b75120c6e1e639bb72dcbeee48b9fa4189
usmanrahim <usmanrahim2000@gmail.com>
2023-04-15 18:22:22 +0500

    Initial

---
+++ cooking.js
@@ -0,0 +1,15 @@
+// Movie Night
+
+// cook prpcorn
+// pour drink
+// start movie
+
+async function MovieNightStarter(){
+    await cookPopcorn();
+    await pourDrink();
+    startMovie();
+}
+
+function cookPopcorn(){
+    return Promise;
+}
---
+++ encryptKey.js
@@ -0,0 +1,20 @@
+const ethers = require("ethers");
+const fs = require("fs-extra");
+require("dotenv").config();
+
+async function main() {
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
+  const encryptedJsonKey = await wallet.encrypt(
+    process.env.PRIVATE_KEY,
+    process.env.PRIVATE_KEY_PASSWORD
+  );
+  console.log(encryptedJsonKey);
+  fs.writeFileSync("./.encryptedKey.json", encryptedJsonKey);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
